bd879ef4c2dc6c11fe29c045d659597b17505510
deru <derupratama03@gmail.com>
2026-05-06 12:54:10 +0700

    Menginisialisasi Tailwind dan Setup View Dasar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-//
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
+
+import ApexCharts from 'apexcharts';
+window.ApexCharts = ApexCharts;
+
+import Swiper from 'swiper/bundle';
+window.Swiper = Swiper;
+
+import 'swiper/css/bundle';
+
+import SimpleBar from 'simplebar';
+window.SimpleBar = SimpleBar;
