7230986e23360133db6a997f00d587d998d8abb0
deru <derupratama03@gmail.com>
2026-05-12 00:19:48 +0700

    Mengganti template lama ke template baru

--- vite.config.js
+++ vite.config.js
@@ -10,11 +10,4 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
 });
-
-
